46e2ae59be9239c1bf2423282d081bd18a489af5
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-05 16:34:19 -0700

    lmoush cruzy

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* check_moush - Checks if a singly-linked list contains a cycle.
+* @list: A singly-linked list.
+*
+* Return: If there is no cycle - 0.
+*         If there is a cycle - 1.
+*/
+int check_moush(listint_t *list)
+{
+listint_t *fair, *hell;
+
+if (list == NULL || list->next == NULL)
+return (0);
+
+fair = list->next;
+hell = list->next->next;
+
+while (fair && hell && hell->next)
+{
+if (fair == hell)
+return (1);
+
+fair = fair->next;
+hell = hell->next->next;
+}
+
+return (0);
+}
