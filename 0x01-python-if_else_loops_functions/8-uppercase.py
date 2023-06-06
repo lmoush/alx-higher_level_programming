07fe622c386086e510a1d186680faf08af64982a
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-06 16:05:31 -0700

    lmoush MIAW

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(moush):
-    for i in moush:
+def uppercase(str):
+    for i in str:
         if ord('a') <= ord(i) <= ord('z'):
         i = chr(ord(i) - 32)
         print("{:s}".format(i), end="")
