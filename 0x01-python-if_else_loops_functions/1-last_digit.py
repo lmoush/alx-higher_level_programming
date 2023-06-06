79896f55eb2bcf4f9bd30904976b92b48f796a3c
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-06 15:32:54 -0700

    lmoush MIAW

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,12 +4,11 @@ number = random.randint(-10000, 10000)
 cnv_str = str(number)
 x = cnv_str[-1]
 if number > 0 or number == 0:
-if int(M) > 5:
-print(f"Last digit of {number} is {M} and is greater than 5")
-elif int(M) == 0:
-print(f"Last digit of {number} is {M} and is 0")
-elif int(M) < 6 and int(M) != 0:
-print(f"Last digit of {number} is {M} and is less than 6 and not 0")
+if int(x) > 5:
+print(f"Last digit of {number} is {x} and is greater than 5")
+elif int(x) == 0:
+print(f"Last digit of {number} is {x} and is 0")
+elif int(x) < 6 and int(x) != 0:
+print(f"Last digit of {number} is {x} and is less than 6 and not 0")
 else:
-print(f"Last digit of {number} is -{M} and is less than 6 and not 0")
-
+print(f"Last digit of {number} is -{x} and is less than 6 and not 0")
