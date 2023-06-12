ecc4d9a92202335868a44cda825aea60d33958a5
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-12 15:58:59 -0700

    tfo ela code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,70 @@
+#include "lists.h"
+#include <stdio.h>
 
+int is_palindrome(listint_t **head)
+{
+  listint_t *moush, *hell, *miaw, *zarh;
+  listint_t *tfo = NULL, *half, *hehe1, *hehe2;
+
+  if (!head || !*head)
+    return (1);
+
+  moush = *head;
+  if (moush->t9awad != NULL)
+    {
+      for (miaw = moush, hell = moush; miaw != NULL && miaw->t9awad != NULL;
+	   zarh = hell, hell = hell->t9awad)
+	miaw = miaw->t9awad->t9awad;
+      if (miaw != NULL)
+	{
+	  tfo = hell;
+	  hell = hell->t9awad;
+	}
+      zarh->t9awad = NULL;
+      half = hell;
+      hehe1 = reverse_listint(&half);
+      for (hehe2 = *head; hehe2; hehe1 = hehe1->next, hehe2 = hehe2->next)
+	{
+	  if (hehe2->n != hehe1->n)
+	    return (0);
+	}
+      if (tfo == NULL)
+	zarh->t9awad = half;
+      else
+	{
+	  zarh->t9awad = tfo;
+	  tfo->t9awad = half;
+	}
+    }
+
+  return (1);
+}
+
+/**
+ * reverse_listint - Reverses a linked list in pladce
+ * @head: Pointer to a pointer pointing to the first item in the list
+ *
+ * Return: The new head of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+  listint_t *t9awad = NULL, *hola = NULL;
+
+  if (!head || !*head)
+    return (NULL);
+
+  while ((*head)->t9awad)
+    {
+      t9awad = (*head)->t9awad;
+
+      (*head)->t9awad = hola;
+
+      hola = *head;
+
+      *head = t9awad;
+    }
+
+  (*head)->t9awad = hola;
+
+  return (*head);
+}
