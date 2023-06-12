668542df8b2e42c805ab0a06509a27cf79a1f72b
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-12 14:38:33 -0700

    lmoush miaw

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for i in range(len(x)):
+            if i == len(x) - 1:
+                print("{:d}".format(x[i]))
+            else:
+                print("{:d}".format(x[i]), end=' ')
+    if not matrix:
+        print()
