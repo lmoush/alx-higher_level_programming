39e805454c5394c7ea2591af849a3a4eccc5866d
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-12 16:12:25 -0700

    tfo ela code

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,70 +1,26 @@
-#include "lists.h"
-#include <stdio.h>
-
-int is_palindrome(listint_t **head)
-{
-  listint_t *nhead, *tort, *hare, *ptort;
-  listint_t *cut = NULL, *half, *it1, *it2;
-
-  if (!head || !*head)
-    return (1);
-
-  nhead = *head;
-  if (nhead->next != NULL)
-    {
-      for (hare = nhead, tort = nhead; hare != NULL && hare->next != NULL;
-	   ptort = tort, tort = tort->next)
-	hare = hare->next->next;
-      if (hare != NULL)
-	{
-	  cut = tort;
-	  tort = tort->next;
-	}
-      ptort->next = NULL;
-      half = tort;
-      it1 = reverse_listint(&half);
-      for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
-	{
-	  if (it2->n != it1->n)
-	    return (0);
-	}
-      if (cut == NULL)
-	ptort->next = half;
-      else
-	{
-	  ptort->next = cut;
-	  cut->next = half;
-	}
-    }
-
-  return (1);
-}
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
 /**
- * reverse_listint - Reverses a linked list in pladce
- * @head: Pointer to a pointer pointing to the first item in the list
+ * print_python_list_info - Print list info about Python
  *
- * Return: The new head of the reversed list
+ * @p: Pyobjext pointer
+ *
+ * Return: void
  */
-listint_t *reverse_listint(listint_t **head)
+
+void print_python_list_info(PyObject *p)
 {
-  listint_t *next = NULL, *prev = NULL;
+	Py_ssize_t count;
+	Py_ssize_t length = PyList_Size(p);
+	PyListObject *pObj = (PyListObject *)p;
 
-  if (!head || !*head)
-    return (NULL);
+	printf("[*] Size of the Python List = %li\n", length);
+	printf("[*] Allocated = %ld\n", pObj->allocated);
 
-  while ((*head)->next)
-    {
-      next = (*head)->next;
-
-      (*head)->next = prev;
-
-      prev = *head;
-
-      *head = next;
-    }
-
-  (*head)->next = prev;
-
-  return (*head);
+	for (count = 0; count < length; count++)
+	{
+		printf("Element %ld: %s\n", count, Py_TYPE(pObj->ob_item[count])->tp_name);
+	}
 }
