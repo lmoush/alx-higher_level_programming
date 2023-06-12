3b8bc6a479fb31976df3dff6ca85287779182ff9
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-12 15:47:38 -0700

    ser t9awed

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,83 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include "lists.h"
 
+/**
+  * is_palindrome - Checks if a singly linked list is a palindrome
+  * @head: The head of the singly linked list
+  *
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *moush, *hell, *miaw, *zarh;
-  listint_t *tfo = NULL, *half, *hehe1, *hehe2;
+    listint_t *start = NULL, *end = NULL;
+    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
 
-  if (!head || !*head)
-    return (1);
+    if (head == NULL)
+        return (0);
 
-  moush = *head;
-  if (moush->t9awad != NULL)
+    if (*head == NULL)
+        return (1);
+    
+    start = *head;
+    len = listint_len(start);
+    len_cyc = len * 2;
+    len_list = len_cyc - 2;
+    end = *head;
+
+    for (; i < len_cyc; i = i + 2)
     {
-      for (miaw = moush, hell = moush; miaw != NULL && miaw->t9awad != NULL;
-	   zarh = hell, hell = hell->t9awad)
-	miaw = miaw->t9awad->t9awad;
-      if (miaw != NULL)
-	{
-	  tfo = hell;
-	  hell = hell->t9awad;
-	}
-      zarh->t9awad = NULL;
-      half = hell;
-      hehe1 = reverse_listint(&half);
-      for (hehe2 = *head; hehe2; hehe1 = hehe1->next, hehe2 = hehe2->next)
-	{
-	  if (hehe2->n != hehe1->n)
-	    return (0);
-	}
-      if (tfo == NULL)
-	zarh->t9awad = half;
-      else
-	{
-	  zarh->t9awad = tfo;
-	  tfo->t9awad = half;
-	}
+        if (start[i].n != end[len_list].n)
+            return (0);
+
+        len_list = len_list - 2;
     }
 
-  return (1);
+    return (1);
+}
+
+/**
+  * get_nodeint_at_index - Gets a node from a linked list
+  * @head: The head of the linked list
+  * @index: The index to find in the linked list
+  *
+  * Return: The specific node of the linked list
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current = head;
+	unsigned int iter_times = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			if (iter_times == index)
+				return (current);
+
+			current = current->next;
+			++iter_times;
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+  * slistint_len - Counts the number of elements in a linked list
+  * @h: The linked list to count
+  *
+  * Return: Number of elements in the linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	int lenght = 0;
+
+	while (h != NULL)
+	{
+		++lenght;
+		h = h->next;
+	}
+
+	return (lenght);
 }
