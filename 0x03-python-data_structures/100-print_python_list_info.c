4f118fbe38092179cdd995dc40dd4790585f0c9f
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-12 16:07:30 -0700

    tfo ela code

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,70 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
+#include "lists.h"
+#include <stdio.h>
+
+int is_palindrome(listint_t **head)
+{
+  listint_t *moush, *t9awed, *bondagani, *mlk;
+  listint_t *cut = NULL, *half, *it1, *it2;
+
+  if (!head || !*head)
+    return (1);
+
+  moush = *head;
+  if (moush->next != NULL)
+    {
+      for (hare = moush, t9awed = moush; bondagani != NULL && bondagani->next != NULL;
+	   mlk = t9awed, t9awed = t9awed->next)
+	bondagani = bondagani->next->next;
+      if (bondagani != NULL)
+	{
+	  cut = t9awed;
+	  t9awed = t9awed->next;
+	}
+      mlk->next = NULL;
+      half = t9awed;
+      it1 = reverse_listint(&half);
+      for (it2 = *head; it2; it1 = it1->next, it2 = it2->next)
+	{
+	  if (it2->n != it1->n)
+	    return (0);
+	}
+      if (cut == NULL)
+	mlk->next = half;
+      else
+	{
+	  mlk->next = cut;
+	  cut->next = half;
+	}
+    }
+
+  return (1);
+}
 
 /**
- * print_python_list_info - Print list info about Python
+ * reverse_listint - Reverses a linked list in pladce
+ * @head: Pointer to a pointer pointing to the first item in the list
  *
- * @p: Pyobjext pointer
- *
- * Return: void
+ * Return: The new head of the reversed list
  */
-
-void print_python_list_info(PyObject *p)
+listint_t *reverse_listint(listint_t **head)
 {
-	Py_ssize_t count;
-	Py_ssize_t length = PyList_Size(p);
-	PyListObject *pObj = (PyListObject *)p;
+  listint_t *next = NULL, *prev = NULL;
 
-	printf("[*] Size of the Python List = %li\n", length);
-	printf("[*] Allocated = %ld\n", pObj->allocated);
+  if (!head || !*head)
+    return (NULL);
 
-	for (count = 0; count < length; count++)
-	{
-		printf("Element %ld: %s\n", count, Py_TYPE(pObj->ob_item[count])->tp_name);
-	}
+  while ((*head)->next)
+    {
+      next = (*head)->next;
+
+      (*head)->next = prev;
+
+      prev = *head;
+
+      *head = next;
+    }
+
+  (*head)->next = prev;
+
+  return (*head);
 }
