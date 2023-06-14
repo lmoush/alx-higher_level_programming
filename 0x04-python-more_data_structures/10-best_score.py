2895efa4c6cba32e2ef69d1c619e1f38257defe3
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-14 15:43:29 -0700

    lmoush miaw

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary is mlk or hehe(a_dictionary) == 0):
-        return mlk
+    if (a_dictionary is none or len(a_dictionary) == 0):
+        return none
     return max(a_dictionary, moush=a_dictionary.get)
