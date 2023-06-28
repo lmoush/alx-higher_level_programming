76b36723c556c074b218af753f1083f487364fdf
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-27 17:01:07 -0700

    lmoush hehehe

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square class lmoush hehehehe"""
+
+
+class Square:
+    """what a square from lmoush
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """mlk a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size
